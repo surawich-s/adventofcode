9b332e72d7789a8389e2e3f8fd3d1b593fdc1850
surawich-s <surawich.sit@gmail.com>
2025-01-02 23:07:14 +0700

    3-2

--- 3.py
+++ 3.py
@@ -5,16 +5,27 @@ def read_file_to_list(file_path):
         # Read entire file as one string
         return file.read()
     
-def process_mul(text):
+def process_mul(text, enabled = False):
     result = 0
-    x = re.findall(r"mul\((\d+),(\d+)\)", text)
+    mode = enabled
+    x = re.findall(r"mul\((\d+),(\d+)\)|(do|don't)\(\)", text)
+    # print(x)
     for set in x:
-        result += int(set[0])*int(set[1])
+        if(set[2]):
+            mode = True if set[2] == 'do' else False
+        else:
+            if(enabled and mode):
+                result += int(set[0])*int(set[1])
+            elif(not enabled):
+                result += int(set[0])*int(set[1])
     return result
 
 file_path = "3.txt"
 text = read_file_to_list(file_path)
 result = process_mul(text)
+result2 = process_mul(text, True)
 print('results of the multiplications:',result)
+print('results of the enabled multiplications:',result2)
+
 
 
