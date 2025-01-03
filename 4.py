d58f59baac1792ac33f04032a814ceb59c5ffe63
surawich-s <surawich.sit@gmail.com>
2025-01-03 23:38:44 +0700

    4

--- 4.py
+++ 4.py
@@ -48,43 +48,31 @@ def word_search(word, puzzle):
 
 def word_search_xmas(puzzle):
     word_count = 0
-    m_count = 0
-    s_count = 0
-    def traverse_puzzle(x,y,word_pos=1):
-        nonlocal m_count, s_count
-        if(x < 0 or x > len(puzzle[0])-1 or y < 0 or y > len(puzzle)-1 or word_pos > 2 or m_count > 2 or s_count > 2):
-            return 0
-        # if(word_pos == 1):
-        #     if(puzzle[y][x] != 'A'):
-        #         return 0
-        #     else:
-        #         print('found A at position:',x,y)
-        if(word_pos == 2):
-            if(puzzle[y][x] == 'M'):
-                m_count += 1
-                print('found M at position:',x,y,'dmcount:', m_count)
-            elif(puzzle[y][x] == 'S'):
-                s_count += 1
-                print('found S at position:',x,y,'dscount:', s_count)
-            else:
-                return 0
-            if(m_count == 2 and s_count == 2):
-                return 1
-        return sum([
-            traverse_puzzle(x+1, y+1, word_pos+1), # southeast
-            traverse_puzzle(x-1, y+1, word_pos+1), # southwest
-            traverse_puzzle(x+1, y-1, word_pos+1), # northeast
-            traverse_puzzle(x-1, y-1, word_pos+1), # northwest
-        ])
+
+    def checkDiagonal(rowA, colA, rowB, colB):
+            a = puzzle[rowA][colA]
+            b = puzzle[rowB][colB]
+            if(a == "M" and b == "S"):
+                return True
+            if(a == "S" and b == "M"):
+                return True
+            return False
+
+    def traverse_puzzle(x,y):
+        nonlocal word_count
+        if(not checkDiagonal(y -1,x -1, y + 1, x +1)):
+            return
+        if(not checkDiagonal(y -1,x +1, y + 1, x -1)):
+            return
+        word_count += 1
+        
 
     for i in range(1,len(puzzle)-1):
         for j in range(1,len(puzzle[0])-1):
             # traverse through puzzle 
             
             if(puzzle[i][j] == 'A'):
-                s_count = 0
-                m_count = 0
-                word_count += traverse_puzzle(j,i)
+                traverse_puzzle(j,i)
             
             # print(word_count, j, i)
     return word_count
