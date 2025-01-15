cb6f14028520afc3e8fe09ba5a022bd4ff574649
surawich-s <surawich.sit@gmail.com>
2025-01-16 04:09:15 +0700

    11-2 solved

--- 11.py
+++ 11.py
@@ -1,31 +1,54 @@
+from collections import defaultdict
+
 def read_file_to_list(file_path):
     stones = []
     # Open and read the file
     with open(file_path, 'r') as file:
         text = file.read()
-        stones = text.strip().split(' ')
+        stones = defaultdict(int)
+        for stone in text.split():
+            stone = int(stone)
+            stones[stone] += 1
     return stones
 
-def change_stone(stone):
+def blink_time(blinks):
+    for i in range(blinks):
+        blink()
+        # print(i+1, stones)
+    return
 
-    if stone == '0':
-        return ['1']
-    if len(stone) % 2 == 0:
-        mid_position = len(stone)//2
-        first = str(int(stone[0:mid_position]))
-        second = str(int(stone[mid_position:]))
-        return [first, second]
-    return [str(int(stone)*2024)]
+def blink():
+    tmp_stone = dict(stones)
+
+    for stone, count in tmp_stone.items():
+        if count == 0: continue
+        if stone == 0:
+            stones[1] += count
+            stones[0] -= count
+        elif len(str(stone)) % 2 == 0:
+            str_stone = str(stone)
+            mid = len(str_stone) // 2
+            first = int(str_stone[:mid])
+            second = int(str_stone[mid:])
+            stones[first] += count
+            stones[second] += count
+            stones[stone] -= count
+        else:
+            stones[stone * 2024] += count
+            stones[stone] -= count
+    return
+    
 
 stones = read_file_to_list('11.txt')
-number_of_blink = 25
+number_of_blink = 75
+# print(stones)
+blink_time(number_of_blink)
+print(f'Stones = {sum(stones.values())}')
 
-for i in range(number_of_blink):
-    new_stones = []
-    for x in stones:
-        stone = change_stone(x)
-        new_stones.extend(stone)
-    stones = new_stones.copy()
-    # print(stones)
-print(stones)
-print(len(stones))
+
+
+
+
+
+
+# print(list)
